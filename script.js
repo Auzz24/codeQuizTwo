ea3d281e05558eb8dd65a3cd0d761b20a202b08f
Austin Miller <austin.miller1424@gmail.com>
2023-03-16 19:45:12 -0500

    started code quiz, got three questions, next need to add timer for score

---
+++ script.js
@@ -0,0 +1,93 @@
+var startBtn = document.getElementById("startBtn");
+var navBar = document.getElementById("navBar");
+
+function startQuiz() {
+  startBtn.addEventListener("click", questionOne);
+}
+
+
+function questionOne() {
+  var startSection = document.getElementById("startQuiz");
+  startSection.remove();
+  var questionOne = document.createElement("div");
+  questionOne.setAttribute("id", "questionOne")
+  var answerOne = document.createElement("button");
+  var answerTwo = document.createElement("button");
+  var answerThree = document.createElement("button");
+  var answerCorrect = document.createElement("button");
+  questionOne.innerText =
+    "Which of the following is an actual JavaScript data type?";
+    answerOne.innerText =
+    "Earth"
+    answerTwo.innerText =
+    "wind"
+    answerThree.innerText =
+    "Fire"
+    answerCorrect.innerText =
+    "Number"
+  navBar.appendChild(questionOne);
+  questionOne.appendChild(answerOne);
+  questionOne.appendChild(answerTwo);
+  questionOne.appendChild(answerThree);
+  questionOne.appendChild(answerCorrect);
+  answerCorrect.setAttribute("id", "answerCorrect");
+  answerOne.setAttribute("id", "answerOne");
+  answerTwo.setAttribute("id", "answerTwo");
+  answerThree.setAttribute("id", "answerThree");
+  answerCorrect.addEventListener("click", questionTwo);
+}
+
+
+function  questionTwo(){
+    var questionOne = document.getElementById("questionOne")
+    questionOne.innerHTML="Hello???"
+    var answerOne = document.createElement("button");
+  var answerTwo = document.createElement("button");
+  var answerThree = document.createElement("button");
+  var answerCorrect = document.createElement("button");
+  questionOne.appendChild(answerOne);
+  questionOne.appendChild(answerTwo);
+  questionOne.appendChild(answerThree);
+  questionOne.appendChild(answerCorrect);
+    answerCorrect.innerHTML= "HHHHHHHH";
+    answerOne.innerHTML= "3434343";
+    answerTwo.innerHTML="rrrrrrr";
+    answerThree.innerHTML="please ework";
+    answerCorrect.setAttribute("id", "answerCorrect");
+    answerOne.setAttribute("id", "answerOne");
+    answerTwo.setAttribute("id", "answerTwo");
+    answerThree.setAttribute("id", "answerThree");
+    answerCorrect.addEventListener("click", questionThree);
+}
+
+function questionThree(){
+    var questionOne = document.getElementById("questionOne")
+    questionOne.innerHTML="TINY?"
+    var answerOne = document.createElement("button");
+  var answerTwo = document.createElement("button");
+  var answerThree = document.createElement("button");
+  var answerCorrect = document.createElement("button");
+  questionOne.appendChild(answerOne);
+  questionOne.appendChild(answerTwo);
+  questionOne.appendChild(answerThree);
+  questionOne.appendChild(answerCorrect);
+    answerCorrect.innerHTML= "Is the best";
+    answerOne.innerHTML= "no good";
+    answerTwo.innerHTML="bad gurl";
+    answerThree.innerHTML="not fun";
+    answerCorrect.setAttribute("id", "answerCorrect");
+    answerOne.setAttribute("id", "answerOne");
+    answerTwo.setAttribute("id", "answerTwo");
+    answerThree.setAttribute("id", "answerThree");
+    answerCorrect.addEventListener("click", endQuiz);
+}
+
+function endQuiz(){
+    var questionOne = document.getElementById("questionOne");
+    questionOne.remove();
+    var highScoreHeader = document.createElement("H1");
+    highScoreHeader.innerHTML = "HIGH SCORES"
+    navBar.appendChild(highScoreHeader);
+}
+
+startQuiz();
